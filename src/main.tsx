a14a0412aa1900f2bf89b2d2ba4b0744b4f419e9
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-06 07:38:39 +0700

    feat: wrap App with custom provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { ProductsProvider } from "./context/ProductsProvider";
+import { CartProvider } from "./context/CartProvider";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ProductsProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductsProvider>
   </React.StrictMode>
 );
